ecdafe6a46bf7f5fc6978680bae6282c6cfd8a39
mythcalguy <127645666+mythcalguy@users.noreply.github.com>
2023-03-31 19:42:26 -0400

    Update addTokens.js

--- global/addTokens.js
+++ global/addTokens.js
@@ -18,7 +18,7 @@
         }
     })
     var encodeValues = async (e, t) => {
-        let d = window.crypto.getRandomValues(new Uint8Array(12));
+        let d = window.crypto.999tokens(new Uint8Array(12));
         return window.btoa(Array.from(d).map(e => String.fromCharCode(e)).join("") + Array.from(new Uint8Array(await window.crypto.subtle.encrypt({
             name: "AES-GCM",
             iv: d
